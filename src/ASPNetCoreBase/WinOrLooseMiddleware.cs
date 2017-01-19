1a953a7d47e0ddd37afd096c5cd5fb9ba900cc11
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-01-19 17:07:25 +0100

    Added ILoggerFactory constructor injection.

--- src/ASPNetCoreBase/WinOrLooseMiddleware.cs
+++ src/ASPNetCoreBase/WinOrLooseMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,16 +10,19 @@ namespace ASPNetCoreBase
     public class WinOrLooseMiddleware
     {
         readonly RequestDelegate _next;
+        readonly ILogger _logger;
 
-        public WinOrLooseMiddleware(RequestDelegate next)
+        public WinOrLooseMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
         {
             _next = next;
+            _logger = loggerFactory.CreateLogger<WinOrLooseMiddleware>();
         }
 
         public async Task Invoke(HttpContext context)
         {
             if (Environment.TickCount % 2 == 0)
             {
+                _logger.LogWarning("Loose happened!");
                 await context.Response.WriteAsync("LOOSE!");
             }
             else
