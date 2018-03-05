f64b5b3ac53e646c95a77fc55580d008abfa5317
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-03-05 18:32:42 +0100

    With authentication.

--- AspNetCoreBase/Startup.cs
+++ AspNetCoreBase/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -28,15 +30,59 @@ namespace AspNetCoreBase
 
         public void ConfigureServices( IServiceCollection services )
         {
+            services.AddOptions()
+                    .AddAuthentication( "SuperCookie" )
+                    .AddCookie( "SuperCookie", options =>
+                    {
+                        options.SlidingExpiration = true;
+                        options.ExpireTimeSpan = TimeSpan.FromHours( 1 );
+                    } );
+            services.Configure<WinOrLooseOptions>( _configuration.GetSection( "WinOrLoose" ) );
         }
+
 
         public void Configure( IApplicationBuilder app, IHostingEnvironment env )
         {
-            if( env.IsDevelopment() )
+            if( env.IsDevelopment() ) app.UseDeveloperExceptionPage();
+
+            app.Use( async ( context, next ) =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-            app.Run( async context => await context.Response.WriteAsync( "Hello World!" ) );
+                if( context.Request.Path.StartsWithSegments( "/login" ) )
+                {
+                    var identity = new ClaimsIdentity();
+                    identity.AddClaim( new Claim( "name", "toto" ) );
+                    ClaimsPrincipal principal = new ClaimsPrincipal( identity );
+                    await context.SignInAsync( "SuperCookie", principal );
+                }
+                else if( context.Request.Path.StartsWithSegments( "/logout" ) )
+                {
+                    await context.SignOutAsync( "SuperCookie" );
+                }
+                await next();
+            } );
+
+app.Use( async ( ctx, next ) =>
+{
+    AuthenticateResult result = await ctx.AuthenticateAsync( "SuperCookie" );
+    if( result.Succeeded )
+    {
+        string name = result
+                        .Principal
+                        .Identities
+                        .First()
+                        .Claims
+                        .Single( c => c.Type == "name" )
+                        .Value;
+        await ctx.Response.WriteAsync( $"Hello {name}{Environment.NewLine}" );
+    }
+    else
+    {
+        await ctx.Response.WriteAsync( "Who the hell are you?" + Environment.NewLine );
+    }
+    await next();
+} );
+
+            app.Run( async context => await context.Response.WriteAsync( "I'm the endware!" ) );
         }
     }
 }
---
+++ AspNetCoreBase/WinOrLooseOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreBase
+{
+    public class WinOrLooseOptions
+    {
+        public int OneOutOf { get; set; } = 2;
+    }
+}
